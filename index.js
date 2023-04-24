c562c1ffb88b9eae5c102dea1c2bbb67a0fa4aa9
bear-muna <bearmuna@gmail.com>
2023-04-23 19:01:28 -0700

    added intern switch case

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const teamApp = async () => {
                         name: 'memberAdd'
                     },
                 ]);
+            team.push(new Manager(managerData.managerName, managerData.managerID, managerData.managerEmail, managerData.managerOfficeNumber));    
             memberApp(managerData.memberAdd);
     }
 }
@@ -78,7 +79,49 @@ const memberApp = async (choice) => {
                         message: "What is the engineer's GitHub username?",
                         name: 'engineerGithub',
                     },
+                    {
+                        type: 'list',
+                        message: "What team member do you want to add?",
+                        choices: ['Engineer', 'Intern', 'Quit'],
+                        name: 'memberAdd',
+                    },
                 ]);
+            team.push(new Engineer(engineerData.engineerName, engineerData.engineerID, engineerData.engineerEmail, engineerData.engineerGithub));
+            memberApp(engineerData.memberAdd);
+            break;
+        case 'Intern':
+            const internData = await inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: "What is the intern's name?",
+                        name: 'internName',
+                    },
+                    {
+                        type: 'input',
+                        message: "What is the intern's ID?",
+                        name: "internID",
+                    },
+                    {
+                        type: 'input',
+                        message: "What is the intern's email?",
+                        name: 'internEmail',
+                    },
+                    {
+                        type: 'input',
+                        message: "What is the intern's school?",
+                        name: 'internSchool',
+                    },
+                    {
+                        type: 'list',
+                        message: "What team member do you want to add?",
+                        choices: ['Engineer', 'Intern', 'Quit'],
+                        choices: 'memberAdd',
+                    }
+                ]);
+            team.push(new Intern(internData.internName, internData.internID, internData.internEmail, internData.internSchool));
+            memberApp(internData.memberAdd);
+        
             
     }
 }
